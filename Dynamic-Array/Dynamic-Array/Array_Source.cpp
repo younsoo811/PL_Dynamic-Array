cddf64a72a0818cb4fe7a0071f688ea7b42555a1
younsoo <younsoo7795@gmail.com>
2022-04-13 17:43:19 +0900

    final

--- Dynamic-Array/Dynamic-Array/Array_Source.cpp
+++ Dynamic-Array/Dynamic-Array/Array_Source.cpp
@@ -29,8 +29,10 @@
 #define STACK_SIZE 10
 
 int SumOfStack = STACK_SIZE;
-int stack[STACK_SIZE];  // 1차원 배열 스택 선언
-int top = -1;               // top 초기화
+int *stack;				// 1차원 배열 스택 선언
+int *stack_B;			// 선언된 스택 백업
+int top = -1;			// top 초기화
+int pop_B;				// pop 값 백업
 
 // 스택이 공백 상태인지 확인하는 연산
 int isEmpty() {
@@ -49,11 +51,18 @@ void push(int item) {
 	if (isFull()) {				// 스택이 포화 상태인 경우
 		SumOfStack += STACK_SIZE;
 		
+		stack = (int*)malloc(SumOfStack * sizeof(int));		//malloc을 이용해 사이즈 20개로 늘림 (처음에 넣던 c의 10개 데이터가 0으로 초기화됨)
+		for (int i = 0; i < SumOfStack-STACK_SIZE; i++) stack[i] = stack_B[i];	//d에 백업했던 c의 값 불러오기
+
 		printf("  --- 스택 %d개", SumOfStack);
 		stack[++top] = item;
+		
 		return;
 	}
-	else stack[++top] = item;	// top을 증가시킨 후 현재 top에 원소 삽입
+	else {
+		stack[++top] = item;	// top을 증가시킨 후 현재 top에 원소 삽입
+		stack_B[top] = stack[top];	//stack 배열 값 백업
+	}
 }
 
 // 스택의 top에서 원소를 삭제하는 연산
@@ -64,13 +73,20 @@ int pop() {
 
 		return 0;
 	}
-	else if (top%STACK_SIZE == 0 && SumOfStack>STACK_SIZE){	//스택사이즈는 최소 10이어야 한다.
+	else if (top % STACK_SIZE == 0 && SumOfStack > STACK_SIZE){	//스택사이즈는 최소 10이어야 한다.
 		SumOfStack -= STACK_SIZE;
+
+		pop_B = stack[top--];	//pop값 백업
+
+		stack = (int*)malloc(SumOfStack * sizeof(int));		//malloc을 이용해 사이즈 20개로 늘림 (처음에 넣던 c의 10개 데이터가 0으로 초기화됨)
+		for (int i = 0; i < SumOfStack; i++) stack[i] = stack_B[i];	//d에 백업했던 c의 값 불러오기
 		
 		printf("  --- 스택 %d개", SumOfStack);
-		return stack[top--];
+		return pop_B;
 	}
-	else return stack[top--];	// 현재 top의 원소를 삭제한 후 top 감소
+	else {
+		return stack[top--];	// 현재 top의 원소를 삭제한 후 top 감소
+	}
 }
 
 // 스택의 원소를 출력하는 연산
@@ -88,6 +104,9 @@ void main(void) {
 	int item;
 	int data;
 
+	stack = (int*)malloc(STACK_SIZE * sizeof(int));	//사이즈 10, 
+	stack_B = (int*)malloc(STACK_SIZE * sizeof(int));	//사이즈 10
+
 	printf("\n** 순차 스택 연산 **\n");
 
 	printf("\n--- 초기 스택 %d 개 ---\n", SumOfStack);
@@ -101,7 +120,7 @@ void main(void) {
 		push(data); printStack();
 		
 	}
-	printf("\n--- 총 스택 %d 개 ---\n", SumOfStack);
+	printf("\n--- push 이후 최종 스택 %d 개 ---\n", SumOfStack);
 
 	for (int i = 0; i < 31; i++){
 		
@@ -116,6 +135,9 @@ void main(void) {
 
 	}
 	printStack();	// 삭제
-	printf("\n\n--- 최종 스택 %d 개 ---\n", SumOfStack);
+	printf("\n\n--- 최종 스택 %d 개 ---\n\n", SumOfStack);
+
+	free(stack);
+	free(stack_B);
 	getchar();
 }
